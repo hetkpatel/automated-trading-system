2a5b57de7909429c40e57b4d2bf2e696f707e27d
Het Patel <hp145371@gmail.com>
2020-02-22 13:25:52 -0600

    Update RSI calculation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,35 +6,42 @@ import java.util.*;
 public class Main {
 
 	// Enter stock tickers to keep track of here
-	static String[] symbols = new String[] {"BABA", "TSLA", "S", "TMUS"};
-	static Map<String, StockData> stocks = new HashMap<String, StockData>();
+//	static String[] symbols = new String[] {"TMUS"};
+//	static Map<String, StockData> stocks = new HashMap<String, StockData>();
+	static String symbol = "TMUS";
+	static StockData stock;
 	
-	static ArrayList<BigDecimal> lastPrice = new ArrayList<BigDecimal>();
+//	static ArrayList<BigDecimal> lastPrice = new ArrayList<BigDecimal>();
+	static BigDecimal lastPrice;
 	
 	public static void main(String[] args) throws IOException {
-		for (int i = 0;i < symbols.length;i++)
-			stocks.put(symbols[i], new StockData(YahooFinance.get(symbols[i])));
+//		for (int i = 0;i < symbols.length;i++)
+//			stocks.put(symbols[i], new StockData(YahooFinance.get(symbols[i])));
+		stock = new StockData(YahooFinance.get(symbol));
+		
+		System.out.println("Count\tPrice\tChange\tGain\tLoss\tAvg Gain\tAvg Loss\tRS\tRSI");
 		
 		Thread thread = new Thread(new Runnable() {
 			public void run() {
 				int counter = 0;
 				while (true) {
 					try {
-						if (lastPrice.size() == 0)
-							for (int i = 0;i < symbols.length;i++)
-								lastPrice.add(i, stocks.get(symbols[i]).price());
-						else {
-							System.out.println(counter);
-							for (int i = 0;i < symbols.length;i++) {
-								BigDecimal price = stocks.get(symbols[i]).price();
-								BigDecimal last = lastPrice.get(Arrays.asList(symbols).indexOf(symbols[i]));
-								System.out.println(symbols[i] + "\t" + price + "\t-->\t" + last);
-								if (price.compareTo(last) == -1) {
-									stocks.get(symbols[i]).appendLoss(price.subtract(last).abs());
-								} else if (price.compareTo(last) == 1) {
-									stocks.get(symbols[i]).appendGain(price.subtract(last).abs());
-								}
-								lastPrice.set(Arrays.asList(symbols).indexOf(symbols[i]), price);
+						if (lastPrice == null) {
+							lastPrice = stock.price();
+							System.out.println(counter + "\t" + lastPrice + "\t");
+						} else {
+							BigDecimal price = stock.price();
+							BigDecimal change = price.subtract(lastPrice);
+							if (change.compareTo(new BigDecimal(0)) == 1) {
+								stock.appendGain(change);
+								System.out.print(counter + "\t" + price + "\t" + change + "\t" + change + "\t\t");
+							} else if (change.compareTo(new BigDecimal(0)) == -1) {
+								stock.appendLoss(change.abs());
+								System.out.print(counter + "\t" + price + "\t" + change + "\t\t" + change.abs() + "\t");
+							}
+							lastPrice = price;
+							if (counter >= 14) {
+								System.out.print(stock.avgGain() + "\t" + stock.avgLoss() + "\t" + stock.rs() + "\t" + stock.rsi());
 							}
 							System.out.println();
 						}
@@ -51,7 +58,3 @@ public class Main {
 		thread.start();
 	}
 }
-
-/**
- * RSI = 100 - (100 / 1 + (avgUp/avgDnw))
- */
--- src/main/java/StockData.java
+++ src/main/java/StockData.java
@@ -28,4 +28,33 @@ class StockData {
   void appendLoss(BigDecimal amount) {
 	  loss.add(amount);
   }
+  
+  BigDecimal avgGain() {
+	  BigDecimal sum = new BigDecimal(0);
+	  for (int i = 0;i < gains.size();i++) {
+		  sum.add(gains.get(i));
+	  }
+	  return sum.divide(new BigDecimal(gains.size()));
+  }
+  
+  BigDecimal avgLoss() {
+	  BigDecimal sum = new BigDecimal(0);
+	  for (int i = 0;i < loss.size();i++) {
+		  sum.add(loss.get(i));
+	  }
+	  return sum.divide(new BigDecimal(loss.size()));
+  }
+  
+  BigDecimal rs() {
+	  return avgGain().divide(avgLoss());
+  }
+  
+  BigDecimal rsi() {
+	  return avgLoss().equals(new BigDecimal(0)) ? new BigDecimal(100) :
+		  new BigDecimal(100).subtract(new BigDecimal(100).divide(rs().add(new BigDecimal(1))));
+  }
 }
+
+/**
+ * RSI = 100 - (100 / 1 + (avgUp/avgDnw))
+ */
